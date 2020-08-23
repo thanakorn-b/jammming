331dd192cf8324bceb3f456186fe38fd64cf01b9
Thanakorn Boonyamanee <thanakornboonyamanee@Thanakorns-MacBook-Pro.local>
2020-08-23 13:30:31 +0700

    setting-structor

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Track from '../Track/Track';
+
+import './TrackList.css';
+
+class TrackList extends React.Component {
+    render() {
+        return(
+            <div className="TrackList">
+                <Track />
+            </div>
+        )
+    }
+}
+
+export default TrackList;
