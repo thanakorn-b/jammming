d50c8c5c5116364ae104a050a8ee1f2089c84ea9
Thanakorn Boonyamanee <thanakornboonyamanee@Thanakorns-MacBook-Pro.local>
2020-08-26 21:46:24 +0700

    Create a Method that Saves the Playlist to a User

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -19,6 +19,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
   addTrack(track) {
     let tracks = this.state.playlistTracks;
@@ -37,6 +38,9 @@ class App extends React.Component {
   updatePlaylistName(name){
     this.setState({playlistName: name});
   }
+  savePlaylist(){
+    let trackURIs = this.state.playlistTracks.map(track => track.uri);
+  }
   render() {
     return (
       <div>
@@ -49,7 +53,8 @@ class App extends React.Component {
             <Playlist playlistName={this.state.playlistName} 
             playlistTracks={this.state.playlistTracks} 
             onRemove={this.removeTrack}
-            onNameChange={this.updatePlaylistName} />
+            onNameChange={this.updatePlaylistName} 
+            onSave={this.savePlaylist} />
           </div>
         </div>
       </div>
